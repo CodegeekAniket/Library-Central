023879af6d3b13bbdf92b9375b4128a72149fa99
Aniket <k.aniket9811@gmail.com>
2024-10-20 23:37:35 +0530

    URL Changed

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://librarymangementsystem.vercel.app/", {
         username,
         password,
       });
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://librarymangementsystem.vercel.app/", {
         username,
         password,
         email,
