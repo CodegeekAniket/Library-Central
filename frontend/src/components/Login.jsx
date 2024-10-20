4617c7ece4ac0c237d5a9c5ea0271b206fdc99d3
Aniket <k.aniket9811@gmail.com>
2024-10-20 23:55:45 +0530

    debugging

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://librarymangementsystem.vercel.app/", {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
         username,
         password,
       });
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://librarymangementsystem.vercel.app/", {
+      const res = await axios.post("http://localhost:5000/api/auth/register", {
         username,
         password,
         email,
